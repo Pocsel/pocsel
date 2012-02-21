8a292f2fcdaba0ef9c155e526ac9d2bc1a75eb38
Dre <dre@trollteam.com>
2012-02-21 11:54:54 +0100

    Tout plein de couleurs pour le fer

--- sdk/plugins/base/cubes/iron.lua
+++ sdk/plugins/base/cubes/iron.lua
@@ -1,10 +1,10 @@
 textures = {
-    top = "iron.png",
-    bottom = "iron.png",
-    left = "iron.png",
-    right = "iron.png",
-    front = "iron.png",
-    back = "iron.png",
+    top = "cubes/iron/top.png",
+    bottom = "cubes/iron/bottom.png",
+    left = "cubes/iron/left.png",
+    right = "cubes/iron/right.png",
+    front = "cubes/iron/front.png",
+    back = "cubes/iron/back.png",
 }
 
 solid = true
