9be60636bbdcd2623bf3cb1e7de4cbe7ce7a418b
plug <plug@trollteam.com>
2012-02-28 19:22:41 +0100

    fix TargetedCubeRenderer.cpp

--- client/game/TargetedCubeRenderer.cpp
+++ client/game/TargetedCubeRenderer.cpp
@@ -33,14 +33,14 @@ namespace Client { namespace Game {
 
     void TargetedCubeRenderer::Render(Common::CubePosition const& pos)
     {
-        static const unsigned char indices[] = {
-            2, 1, 3, 0,
-            6, 5, 7, 4,
-            10, 9, 11, 8,
-            14, 13, 15, 12,
-            18, 17, 19, 16,
-            22, 21, 23, 20
-        };
+//        static const unsigned char indices[] = {
+//            2, 1, 3, 0,
+//            6, 5, 7, 4,
+//            10, 9, 11, 8,
+//            14, 13, 15, 12,
+//            18, 17, 19, 16,
+//            22, 21, 23, 20
+//        };
 
         auto const& camera = this->_game.GetPlayer().GetCamera();
 
@@ -55,7 +55,8 @@ namespace Client { namespace Game {
         this->_vertexBuffer->Bind();
         this->_texture->Bind();
         this->_shaderTexture->Set(*this->_texture);
-        this->_renderer.DrawElements(sizeof(indices), Tools::Renderers::DataType::UnsignedByte, indices, Tools::Renderers::DrawingMode::TrianglesStrip);
+//        this->_renderer.DrawElements(sizeof(indices), Tools::Renderers::DataType::UnsignedByte, indices, Tools::Renderers::DrawingMode::TrianglesStrip);
+        this->_renderer.DrawVertexBuffer(0, 6*6);
         this->_texture->Unbind();
         this->_vertexBuffer->Unbind();
 
@@ -66,46 +67,64 @@ namespace Client { namespace Game {
     {
         const float vertices[] = {
             // front
-            -0.02f, 1.02f, -0.02f,  0.0f, 0.0f,
-            1.02f, 1.02f, -0.02f,  1.0f, 0.0f,
-            1.02f, -0.02f, -0.02f,  1.0f, 1.0f,
-            -0.02f, -0.02f, -0.02f,  0.0f, 1.0f,
+            -0.02f, 1.02f, -0.02f,  1.0f, 1.0f,
+            1.02f, -0.02f, -0.02f,  0.0f, 0.0f,
+            -0.02f, -0.02f, -0.02f,  1.0f, 0.0f,
+
+            1.02f, 1.02f, -0.02f,  0.0f, 1.0f,
+            1.02f, -0.02f, -0.02f,  0.0f, 0.0f,
+            -0.02f, 1.02f, -0.02f,  1.0f, 1.0f,
 
             // back
-            1.02f, 1.02f, 1.0f,  0.0f, 0.0f,
-            -0.02f, 1.02f, 1.02f,  1.0f, 0.0f,
-            -0.02f, -0.02f, 1.02f,  1.0f, 1.0f,
-            1.02f, -0.02f, 1.02f,  0.0f, 1.0f,
-
-            // left
-            -0.02f, 1.02f, -0.02f,  0.0f, 0.0f,
-            -0.02f, 1.02f, 1.02f,  1.0f, 0.0f,
-            -0.02f, -0.02f, 1.02f,  1.0f, 1.0f,
-            -0.02f, -0.02f, -0.02f,  0.0f, 1.0f,
-
-            // right
-            1.02f, 1.02f, 1.0f,  0.0f, 0.0f,
-            1.02f, 1.02f, -0.02f,  1.0f, 0.0f,
-            1.02f, -0.02f, -0.02f,  1.0f, 1.0f,
-            1.02f, -0.02f, 1.02f,  0.0f, 1.0f,
-
-            // bottom
+            1.02f, 1.02f, 1.02f,  1.0f, 1.0f,
             -0.02f, -0.02f, 1.02f,  0.0f, 0.0f,
             1.02f, -0.02f, 1.02f,  1.0f, 0.0f,
-            1.02f, -0.02f, -0.02f,  1.0f, 1.0f,
-            -0.02f, -0.02f, -0.02f,  0.0f, 1.0f,
+
+            -0.02f, 1.02f, 1.02f,  0.0f, 1.0f,
+            -0.02f, -0.02f, 1.02f,  0.0f, 0.0f,
+            1.02f, 1.02f, 1.02f,  1.0f, 1.0f,
+
+            // left
+            -0.02f, 1.02f, 1.02f,  1.0f, 1.0f,
+            -0.02f, -0.02f, -0.02f,  0.0f, 0.0f,
+            -0.02f, -0.02f, 1.02f,  1.0f, 0.0f,
+
+            -0.02f, 1.02f, -0.02f,  0.0f, 1.0f,
+            -0.02f, -0.02f, -0.02f,  0.0f, 0.0f,
+            -0.02f, 1.02f, 1.02f,  1.0f, 1.0f,
+
+            // right
+            1.02f, 1.02f, -0.02f,  1.0f, 1.0f,
+            1.02f, -0.02f, 1.02f,  0.0f, 0.0f,
+            1.02f, -0.02f, -0.02f,  1.0f, 0.0f,
+
+            1.02f, 1.02f, 1.02f,  0.0f, 1.0f,
+            1.02f, -0.02f, 1.02f,  0.0f, 0.0f,
+            1.02f, 1.02f, -0.02f,  1.0f, 1.0f,
+
+            // bottom
+            -0.02f, -0.02f, -0.02f,  1.0f, 1.0f,
+            1.02f, -0.02f, 1.02f,  0.0f, 0.0f,
+            -0.02f, -0.02f, 1.02f,  1.0f, 0.0f,
+
+            1.02f, -0.02f, -0.02f,  0.0f, 1.0f,
+            1.02f, -0.02f, 1.02f,  0.0f, 0.0f,
+            -0.02f, -0.02f, -0.02f,  1.0f, 1.0f,
 
             // top
-            1.02f, 1.02f, 1.0f,  0.0f, 0.0f,
-            -0.02f, 1.02f, 1.02f,  1.0f, 0.0f,
-            -0.02f, 1.02f, -0.02f,  1.0f, 1.0f,
-            1.02f, 1.02f, -0.02f,  0.0f, 1.0f,
+            -0.02f, 1.02f, 1.02f,  1.0f, 1.0f,
+            1.02f, 1.02f, -0.02f,  0.0f, 0.0f,
+            -0.02f, 1.02f, -0.02f,  1.0f, 0.0f,
+
+            1.02f, 1.02f, 1.02f,  0.0f, 1.0f,
+            1.02f, 1.02f, -0.02f,  0.0f, 0.0f,
+            -0.02f, 1.02f, 1.02f,  1.0f, 1.0f,
         };
 
         this->_vertexBuffer = this->_renderer.CreateVertexBuffer().release();
         this->_vertexBuffer->PushVertexAttribute(Tools::Renderers::DataType::Float, Tools::Renderers::VertexAttributeUsage::Position, 3); // position
         this->_vertexBuffer->PushVertexAttribute(Tools::Renderers::DataType::Float, Tools::Renderers::VertexAttributeUsage::TexCoord, 2); // texCoord
-        this->_vertexBuffer->SetData(6*4*3*sizeof(float) + 6*4*2*sizeof(float), vertices, Tools::Renderers::VertexBufferUsage::Static);
+        this->_vertexBuffer->SetData(6*6*3*sizeof(float) + 6*6*2*sizeof(float), vertices, Tools::Renderers::VertexBufferUsage::Static);
     }
 
 }}
