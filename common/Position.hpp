0fcf0d8fb0fe1b65ac1a83c1d09d68e4a4cea462
paps <paps@trollteam.com>
2012-04-03 19:39:02 +0200

    Nettoyage Position encore

--- common/Position.hpp
+++ common/Position.hpp
@@ -24,12 +24,6 @@ namespace Common {
     /* coordonnées entières du cube relatives au chunk contenant cette position */
     inline BaseChunk::CoordsType GetCubeCoordsInChunk(Position const& pos);
 
-    /* position du cube situé à ce CubePosition (non relatif au chunk) */
-    //inline Position GetCubePosition(CubePosition const& cubePos);
-
-    /* coordonnées entières du cube contenant cette position */
-    //inline CubePosition GetCubePosition(Position const& pos);
-
     /* position de l'origine du chunk contenant cette position */
     inline Position GetPositionInChunk(Position const& pos);
 
@@ -39,23 +33,22 @@ namespace Common {
 
     BaseChunk::CoordsType GetChunkCoords(Position const& pos)
     {
-        return BaseChunk::CoordsType(pos.x, pos.y, pos.z) / Common::ChunkSize;
+        return BaseChunk::CoordsType(pos) / Common::ChunkSize;
     }
 
     Position GetChunkPosition(BaseChunk::CoordsType const& chunkCoords)
     {
-        return Position(chunkCoords.x, chunkCoords.y, chunkCoords.z) * Common::ChunkSize;
+        return Position(chunkCoords) * Common::ChunkSize;
     }
 
     Position GetChunkPosition(Position const& pos)
     {
-        BaseChunk::CoordsType coords = GetChunkCoords(pos);
-        return Position(coords.x, coords.y, coords.z) * Common::ChunkSize;
+        return Position(GetChunkCoords(pos)) * Common::ChunkSize;
     }
 
     BaseChunk::CoordsType GetCubeCoords(Position const& pos)
     {
-        return BaseChunk::CoordsType(pos.x, pos.y, pos.z);
+        return BaseChunk::CoordsType(pos);
     }
 
     BaseChunk::CoordsType GetCubeCoordsInChunk(Position const& pos)
@@ -67,21 +60,6 @@ namespace Common {
                 pos.z - coords.z * Common::ChunkSize
                 );
     }
-
-    //Position GetCubePosition(CubePosition const& cubePos)
-    //{
-    //    Position world = GetChunkPosition(cubePos.world);
-    //    return Position(
-    //            world.x + cubePos.chunk.x,
-    //            world.y + cubePos.chunk.y,
-    //            world.z + cubePos.chunk.z
-    //            );
-    //}
-
-    //CubePosition GetCubePosition(Position const& pos)
-    //{
-    //    return CubePosition(GetChunkCoords(pos), GetCubeCoordsInChunk(pos));
-    //}
 
     Position GetPositionInChunk(Position const& pos)
     {
