bcd02cf8bf17ee9bc30bd46f097ad7ede943eeb7
dre <dre@trollteam.com>
2012-04-16 20:38:11 +0200

    Moins de segfaults

--- server/game/map/Map.cpp
+++ server/game/map/Map.cpp
@@ -50,6 +50,7 @@ namespace Server { namespace Game { namespace Map {
     Map::~Map()
     {
         this->_conf.cubes.clear();
+        this->_conf.cubeTypes.clear();
         Tools::debug << "Map::~Map() -- " << this->_conf.name << "\n";
         Tools::Delete(this->_gen);
         Tools::Delete(this->_engine);
