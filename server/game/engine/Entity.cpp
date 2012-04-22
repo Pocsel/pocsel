ca42e823e7672c809594465b35fb7ff668930e39
paps <paps@trollteam.com>
2012-04-22 16:43:06 +0200

    enlevage de Entity::SetType() qui ne devait plus etre la

--- server/game/engine/Entity.cpp
+++ server/game/engine/Entity.cpp
@@ -6,25 +6,17 @@
 namespace Server { namespace Game { namespace Engine {
 
     Entity::Entity(Engine& engine, Uint32 id, EntityType* type) :
-        _self(engine.GetInterpreter().MakeTable())
+        _type(type), _self(engine.GetInterpreter().MakeTable())
     {
         Tools::Lua::Ref metatable = engine.GetInterpreter().MakeTable();
         this->_self.SetMetaTable(metatable);
         this->_self.Set("id", id);
-        this->SetType(type);
+        this->_self.Set("prototype", this->_type->GetPrototype());
+        metatable.Set("__index", this->_type->GetPrototype());
     }
 
     Entity::~Entity()
     {
-    }
-
-    void Entity::SetType(EntityType* type)
-    {
-        this->_type = type;
-        this->_self.Set("prototype", this->_type->GetPrototype());
-        Tools::Lua::Ref metatable = this->_self.GetMetaTable();
-        if (metatable.IsTable())
-            metatable.Set("__index", this->_type->GetPrototype());
     }
 
 }}}
