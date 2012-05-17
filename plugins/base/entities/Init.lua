e48bdd4c5e5085252630994a72856a4c029a25e7
paps <paps@trollteam.com>
2012-05-17 14:54:45 +0200

    tests de certains trucs dans l'API des entites + sauvegarde BeginTransaction

--- plugins/base/entities/Init.lua
+++ plugins/base/entities/Init.lua
@@ -1,92 +1,56 @@
 Server.Entity.Register{
-    entityName = "Init",
-    Spawn = function(self)
-        self:Method()
-        Server.Message.Later(5, self.id, "EventTest", 1)
 
+    entityName = "Init", -- mandatory
 
+    Spawn = function(self) -- constructor
+        print("Spawn()")
+        self.i = 0
+        self:Loop()
+        for i = 0, 3 do
+            Srv.Entity.Spawn("Blob", self.id)
+        end
     end,
-    Method = function(self)
-        print("hey from " .. self.entityName)
-        print("prototype " .. self.prototype.entityName, self.prototype.id)
+
+    Die = function(self) -- destructor
+        print("Die()")
     end,
-    EventTest = function(self, nbCalls)
-        print(nbCalls)
-        Server.Message.Later(5, self.id, "EventTest", nbCalls, self.id, "Notify", "Test!")
+
+    Save = function(self) -- save to database
+        print("Save()")
+        self.storage.i = self.i
     end,
-    Notify = function(self, arg, result)
-        print(arg, result.entityId, result.success)
-        Server.Message.Later(5, 12, "ca va pas marcher", nil, self.id, "NotifyError")
+
+    Load = function(self) -- load from database
+        print("Load()")
+        self.i = self.storage.i
     end,
-    NotifyError = function(self, arg, r)
-        print("NotifyError: " .. tostring(arg) .. ", " .. r.entityId .. ", " .. tostring(r.success))
+
+    Loop = function(self)
+        self.i = self.i + 1
+        Srv.Message.Later(5, self.id, "Loop")
     end,
+
+    GetCounter = function(self)
+        return self.i
+    end,
+
 }
 
---Server.Entity.Register{
---    name = "Init",
---
---    Spawn = function(entity, this, args, spawnerId)
---        -- entity n'est pas sauve a la fermeture du serveur
---        print("Entity Init du plugin base.")
---        S.Event.CallLater(2, this.id, "_EventTest", { biteBite = "sdfgsdfg" })
---        S.Event.SelfCall("_EventTest", { sdfg = 12 })
---        entity._EventTest(entity, this, {}, this.id)
---        S.Event.SelfCallLater(60, "_EventTest", { sdgffff = 14 })
---        entity.maxiTable = {}
---
---        entity.storage.maxiTable = entity.maxiTable
---    end,
---
---    Save = function(entity, this)
---        this.storage.preciousSavedItem = this.itemId
---    end,
---
---    Load = function(entity, this)
---        this.itemId = this.storage.preciousSavedItem
---    end,
---
---    Die = function(entity, this, args, killerId)
---        print("No!!")
---    end,
---
---    _EventTest = function(entity, this, args, callerId)
---        print(args.biteBite);
---        S.Event.CallLater(2, this.id, "_EventTest", { biteBite = args.biteBite .. "TEST" })
---        S.Event.Call(this.id, "_EventTest", { arg1 = "sdgsdg" })
---        S.Entity.Spawn("Init", { test = "test" }, this.id, "_InitRespawned", { info = 6.44 })
---        S.Entity.SpawnFromPlugin("Dog", "dog-plugin", { arg1 = "!!" }, this.id, "_DogSpawned", { bite = "8==D" })
---        S.Entity.Kill(this.id, { reason = "none :p" }, this.id, "_KillCallback", { hey = -12 })
---    end,
---
---    _InitRespawned = function(entity, this, args, spawnedId)
---    end,
---
---    _KillCallback = function(entity, this, args)
---    end,
---}
---
---
---Server.Entity.Register{
---    name = "Bite",
---    Spawn = function(self)
---        self:_Method()
---        self.type.name
---        self.bite = 12
---    end,
---    _Method = function(self)
---    end,
---}
---
---Server.Entity.Register{
---    name = "Bite",
---    Spawn = function()
---        self.type._Method()
---    end,
---    _Method = function()
---    end,
---}
---
---function Account:Deposit(amount)
---    self.balance = self.balance + amount
---end
+Server.Entity.Register{
+
+    entityName = "Blob",
+
+    Spawn = function(self, initEntity)
+        self:Loop(initEntity)
+    end,
+
+    Loop = function(self, initEntity)
+        Srv.Message.Later(5 + math.random() * 20, initEntity, "GetCounter", nil, self.id, "GotCounter")
+    end,
+
+    GotCounter = function(self, arg, result)
+        print(result.ret)
+        self:Loop(result.entityId)
+    end,
+
+}
