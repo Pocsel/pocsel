9c25ef9079ed18c40571a46b851984cdb57c5aff
plug <plug@trollteam.com>
2012-06-14 20:47:10 +0200

    +bullet, link ok avec le serveur

---
+++ bullet/bullet-all.hpp
@@ -0,0 +1,9 @@
+#ifndef __BULLET_BULLET_ALL_HPP__
+#define __BULLET_BULLET_ALL_HPP__
+
+#define BT_USE_DOUBLE_PRECISION
+
+#include "btBulletCollisionCommon.h"
+#include "btBulletDynamicsCommon.h"
+
+#endif
