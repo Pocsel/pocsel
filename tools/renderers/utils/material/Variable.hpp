bc2503fd5a47001d0052f1ef914565271fd8bde3
paps <paps@trollteam.com>
2012-06-15 18:56:01 +0200

    enlever du code pour que ca marche

--- tools/renderers/utils/material/Variable.hpp
+++ tools/renderers/utils/material/Variable.hpp
@@ -105,32 +105,32 @@ namespace Tools { namespace Renderers { namespace Utils { namespace Material {
     template<class T>
     inline void Material::_SetValue(std::string const& key, T value)
     {
-        auto it = this->_variables.find(key);
+        //auto it = this->_variables.find(key);
 
         Variable<T>* shaderVariable;
-        if (it == this->_variables.end())
-        {
+        //if (it == this->_variables.end())
+        //{
             shaderVariable = &this->GetVariable<T>(key);
-            this->_variables[key].reset(shaderVariable);
-        }
-        else
-            shaderVariable = reinterpret_cast<Variable<T>*>(it->second.get());
+            //this->_variables[key].reset(shaderVariable);
+        //}
+        //else
+        //    shaderVariable = reinterpret_cast<Variable<T>*>(it->second.get());
         shaderVariable->Set(value);
     }
 
     template<class T>
     inline void Material::_SetValue(std::string const& key, std::shared_ptr<T> value)
     {
-        auto it = this->_variables.find(key);
+        //auto it = this->_variables.find(key);
 
         Variable<T>* shaderVariable;
-        if (it == this->_variables.end())
-        {
+        //if (it == this->_variables.end())
+        //{
             shaderVariable = &this->GetVariable<T>(key);
-            this->_variables[key].reset(shaderVariable);
-        }
-        else
-            shaderVariable = reinterpret_cast<Variable<T>*>(it->second.get());
+            //this->_variables[key].reset(shaderVariable);
+        //}
+        //else
+        //    shaderVariable = reinterpret_cast<Variable<T>*>(it->second.get());
         shaderVariable->Set(value);
     }
 
