a795fe0ccaf70c5c54bf9678d02c3d631576e020
dre <dre@trollteam.com>
2012-06-29 19:10:27 +0200

    Compilation ok, plus de warnings avec bullet sous windows

--- bullet/bullet-all.hpp
+++ bullet/bullet-all.hpp
@@ -1,9 +1,18 @@
 #ifndef __BULLET_BULLET_ALL_HPP__
 #define __BULLET_BULLET_ALL_HPP__
 
+#ifdef _MSC_VER
+# pragma warning(push)
+# pragma warning(disable: 4244 4305)
+#endif
+
 #define BT_USE_DOUBLE_PRECISION
 
 #include "btBulletCollisionCommon.h"
 #include "btBulletDynamicsCommon.h"
+
+#ifdef _MSC_VER
+# pragma warning(pop)
+#endif
 
 #endif
