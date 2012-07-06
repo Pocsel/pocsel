49c04f80e04a7aa08e35e2b87b089e7a2a4ec4fe
dre <dre@trollteam.com>
2012-07-06 19:32:05 +0200

    LightManager presque terminé

---
+++ client/graphics/LightManager.hpp
@@ -0,0 +1,99 @@
+#ifndef __CLIENT_GRAPHICS_LIGHTMANAGER_HPP__
+#define __CLIENT_GRAPHICS_LIGHTMANAGER_HPP__
+
+#include "tools/renderers/utils/light/LightRenderer.hpp"
+#include "common/Position.hpp"
+
+namespace Tools {
+    class IRenderer;
+    namespace Lua {
+        class Interpreter;
+    }
+    namespace Renderers { namespace Utils {
+        class GBuffer;
+    }}
+}
+
+namespace Common {
+    struct Camera;
+}
+
+namespace Client { namespace Game {
+    class Game;
+}}
+
+namespace Client { namespace Graphics {
+
+    class LightManager : private boost::noncopyable
+    {
+    public:
+        class DirectionnalLight : private boost::noncopyable
+        {
+        private:
+            LightManager& _lightManager;
+            Tools::Renderers::Utils::Light::DirectionnalLight _internal;
+        public:
+            bool visible;
+
+        public:
+            DirectionnalLight(LightManager& _lightManager);
+            ~DirectionnalLight() { this->_lightManager._Unregister(*this); }
+
+            glm::vec3 const& GetAmbientColor() const { return this->_internal.ambientColor; }
+            glm::vec3 const& GetDiffuseColor() const { return this->_internal.diffuseColor; }
+            glm::vec3 const& GetSpecularColor() const { return this->_internal.specularColor; }
+            glm::vec3 const& GetDirection() const { return this->_internal.direction; }
+            void SetAmbientColor(glm::vec3 color) { this->_internal.ambientColor = color; }
+            void SetDiffuseColor(glm::vec3 color) { this->_internal.diffuseColor = color; }
+            void SetSpecularColor(glm::vec3 color) { this->_internal.specularColor = color; }
+            void SetDirection(glm::vec3 direction) { this->_internal.direction = direction; }
+
+            Tools::Renderers::Utils::Light::DirectionnalLight& GetInternal() { return this->_internal; }
+        };
+
+        class PointLight : private boost::noncopyable
+        {
+        private:
+            LightManager& _lightManager;
+            Tools::Renderers::Utils::Light::PointLight _internal;
+        public:
+            Common::Position position;
+            bool visible;
+
+        public:
+            PointLight(LightManager& lightManager);
+            ~PointLight() { this->_lightManager._Unregister(*this); }
+
+            glm::vec3 const& GetDiffuseColor() const { return this->_internal.diffuseColor; }
+            glm::vec3 const& GetSpecularColor() const { return this->_internal.specularColor; }
+            float GetRange() const { return this->_internal.range; }
+            void SetDiffuseColor(glm::vec3 color) { this->_internal.diffuseColor = color; }
+            void SetSpecularColor(glm::vec3 color) { this->_internal.specularColor = color; }
+            void SetRange(float range) { this->_internal.range = range; }
+
+            Tools::Renderers::Utils::Light::PointLight& GetInternal() { return this->_internal; }
+        };
+
+    private:
+        Game::Game& _game;
+        Tools::IRenderer& _renderer;
+        Tools::Renderers::Utils::Light::LightRenderer _lightRenderer;
+        std::list<std::unique_ptr<DirectionnalLight>> _directionnals;
+        std::list<std::unique_ptr<PointLight>> _points;
+
+    public:
+        LightManager(Game::Game& game);
+
+        void Render(Common::Camera const& camera, Tools::Renderers::Utils::GBuffer& gbuffer);
+        DirectionnalLight& CreateDirectionnalLight();
+        PointLight& CreatePointLight();
+
+    private:
+        void _Unregister(DirectionnalLight& light);
+        void _Unregister(PointLight& light);
+        void _RegisterLua(Tools::Lua::Interpreter& interpreter);
+    };
+
+}}
+
+#endif
--- common/Camera.hpp
+++ common/Camera.hpp
@@ -28,9 +28,14 @@ namespace Common {
         {
         }
 
-        glm::detail::tmat4x4<float> GetViewMatrix() const
+        glm::mat4 GetViewMatrix() const
         {
-            return glm::lookAt<float>(glm::fvec3(0), this->direction, glm::fvec3(0, 1, 0));
+            return glm::lookAt(glm::vec3(0.0f), this->direction, glm::vec3(0.0f, 1.0f, 0.0f));
+        }
+
+        glm::dmat4 GetAbsoluteViewProjectionMatrix() const
+        {
+            return glm::dmat4x4(this->projection) * glm::lookAt(this->position, this->position + glm::dvec3(this->direction), glm::dvec3(0.0, 1.0, 0.0));
         }
     };
 
