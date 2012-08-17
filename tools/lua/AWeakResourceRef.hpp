ebde65bb47d974b498d2018d094d56c0b01135ac
paps <paps@trollteam.com>
2012-08-18 00:15:07 +0200

    des tests pour la serialization de weak ptr

--- tools/lua/AWeakResourceRef.hpp
+++ tools/lua/AWeakResourceRef.hpp
@@ -5,8 +5,9 @@ namespace Tools { namespace Lua {
 
     /*
      * Struct simple contenant le minimum de données permettant de retrouver une resource.
-     * Doit être copiable.
-     * La classe fille doit implémenter l'opérateur <.
+     * DOIT être copiable.
+     * La classe fille DOIT implémenter l'opérateur <.
+     * La classe fille DOIT implémenter un constructeur par défaut qui initialise la weak ref comme étant invalide.
      */
     template<typename ManagerType> struct AWeakResourceRef
     {
@@ -17,6 +18,7 @@ namespace Tools { namespace Lua {
         virtual void Invalidate(ManagerType const&) = 0;
         virtual Ref GetReference(ManagerType const&) const = 0;
         virtual std::string Serialize(ManagerType const&) const { return "return nil"; }
+        virtual void TryToLoad(ManagerType const&) {}
 
         bool IsLoaded() const { return this->_isLoaded; }
         void SetLoaded(bool isLoaded = true) { this->_isLoaded = isLoaded; }
